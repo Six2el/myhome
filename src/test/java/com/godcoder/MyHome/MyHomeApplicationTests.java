0cee681c36bdb39804067f3c75bd52644ec425a7
yuriz <yurizoa123@naver.com>
2022-02-19 20:31:03 +0900

    init

---
+++ src/test/java/com/godcoder/MyHome/MyHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godcoder.MyHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
